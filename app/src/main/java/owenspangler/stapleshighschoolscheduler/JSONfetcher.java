6b6143752594537484ef84c8e5ce34408286ba83
ospangler <39440006+ospangler@users.noreply.github.com>
2018-05-28 18:56:16 -0400

    Moving Everything not UI to async task
    What could go wrong?!?

--- app/src/main/java/owenspangler/stapleshighschoolscheduler/JSONfetcher.java
+++ app/src/main/java/owenspangler/stapleshighschoolscheduler/JSONfetcher.java
@@ -2,11 +2,11 @@ package owenspangler.stapleshighschoolscheduler;
 ///
 import android.os.AsyncTask;
 import android.util.Log;
-import org.json.JSONObject;
+
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONException;
-import java.io.FileReader;
-import java.io.LineNumberReader;
+
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,71 +14,180 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.net.HttpURLConnection;
+import java.util.Calendar;
 ///
-///Portions of Below Code are taken or derived from code made by Abhishek Panwar, 2017///
+///Portions of Below Code is derived from code made by Abhishek Panwar, 2017///
 ///The original code can be found at https://github.com/panwarabhishek345/Receive-JSON-Data///
-public class JSONfetcher extends AsyncTask<Void,Void,Void> {
-    ///
-    String dayletter = "";
-    int month;
-    int day;
-    //String newscheduleformatSTR;
-    String starttimesSTR;
-    String endtimesSTR;
-    //int[] newscheduleformat;
-    String newscheduleformat;
-    int[][] starttimes;
-    int[][] endtimes;
-    String notice;
-    String linktopdf;
-    String[] arr;
+
+class JSONfetcher extends AsyncTask<Void,Void,Void> {
     String data = "";
+    ////
+    String jsonDayLetter = "";
+    int jsonMonth = -1;
+    int jsonDay = -1;
+    int[] jsonNewScheduleFormat;
+    int[][] jsonStartTimes;
+    int[][] jsonEndTimes;
+    String jsonNotice;
+
+
+    // String temperature = JSONObject(html).getString("name");///////FIX AND ADD HTML CODE
+    //TimeZone tz;
+    boolean passingTime = false;
+    boolean noSchool = false;
+
+    Calendar cal = Calendar.getInstance();
+    int currentDayNum = cal.get(Calendar.DAY_OF_MONTH);
+    int currentDayDay = cal.get(Calendar.DAY_OF_WEEK);
+    int currentMonth = cal.get(Calendar.MONTH);
+
+    int currentHour = cal.get(Calendar.HOUR_OF_DAY);
+    //int currentHour = 8;
+    int currentMinute = cal.get(Calendar.MINUTE);
+    //int currentMinute = 15;
+    int currentSecond = cal.get(Calendar.SECOND);
+
+
+
+
+    ///
+
 
     @Override
-    public Void doInBackground(Void... voids) {
-        try {
-            URL url = new URL("https://ospangler.github.io/schedulechangedatabase.json");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = "";
+    protected Void doInBackground(Void...voids) {
+        RetrieveJson();
+        GetInfoFromJSON(data);
 
-            while (true) {
-                line = bufferedReader.readLine();
-                if (line == null) {
-                    break;
-                }
-                data = data + line;
+
+        return null;///redundant
+    }
+void RetrieveJson(){
+    try {
+        URL url = new URL("https://ospangler.github.io/schedulechangedatabase.json");
+        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = httpURLConnection.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+
+        while (true) {
+            line = bufferedReader.readLine();
+            if (line == null) {
+                break;
             }
-            //println(data);
-            Log.d("helpme", data);
-            JSONObject JO = new JSONObject(data);
-            dayletter = JO.getString("dayletter");
-            Log.d("dayletter", dayletter);
-            //println(dayletter);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+            data = data + line;
         }
 
-        return null;
+
+    } catch (MalformedURLException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+    void GetInfoFromJSON(String inputdata){
+        try {
+            JSONObject JO = new JSONObject(inputdata);
+            jsonDayLetter= JO.getString("dayletter");
+            jsonMonth = JO.getInt("month");
+            jsonDay = JO.getInt("day");
+            ////
+            // Retrieve number array from JSON object "newscheduleformat".
+            JSONArray NSFarray = JO.optJSONArray("newscheduleformat");
+            if (NSFarray == null) { /*ENTER SOME ERROR CODE HERE*/ }
+            jsonNewScheduleFormat = new int[NSFarray.length()];
+            for (int i = 0; i < NSFarray.length(); ++i) {
+                jsonNewScheduleFormat[i] = NSFarray.optInt(i);
+            }
+            ///
+            int[] tempJsonStartTimesHour = getArrayFromJSON("startimeshour");
+            int[] tempJsonStartTimesMinute = getArrayFromJSON("starttimesminute");
+            int[] tempJsonEndTimesHour = getArrayFromJSON("endtimeshour");
+            int[] tempJsonEndTimesMinute = getArrayFromJSON("endtimesminute");
+
+        }catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    int[] getArrayFromJSON(String jsonID) {
+        try {
+            JSONObject tempJO = new JSONObject(data);
+            JSONArray tempJsonArray = tempJO.optJSONArray(jsonID);
+            if (tempJsonArray == null) { /*ENTER SOME ERROR CODE HERE*/ }
+            int[] output = new int[tempJsonArray.length()];
+            for (int i = 0; i < tempJsonArray.length(); ++i) {
+                output[i] = tempJsonArray.optInt(i);
+            }
+            return output;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        //return null;
+    }
+    int[] ScheduleFormat(char inputDayType) {//edit here for schedule changes
+
+        if (inputDayType == 'a') {
+            int[] tempA = {1, 2, 3, 5, 8, 7}; //A day
+            return tempA;
+        } else if (inputDayType == 'b') {
+            int[] tempB = {2, 3, 4, 6, 7, 8}; //B day
+            return tempB;
+        } else if (inputDayType == 'c') {
+            int[] tempC = {3, 4, 1, 7, 6, 5}; //C day
+            return tempC;
+        } else if (inputDayType == 'd') {
+            int[] tempD = {4, 1, 2, 8, 5, 6}; //D day
+            return tempD;
+        } else {
+            //CALL FUNCTION TO PULL DATA FROM SERVER HERE
+            return null;
+        }
     }
 
-    public String geter() {
-        return dayletter;
+    int PeriodNumber() {
+        int i = 0; //array position
+        passingTime = false;//If set to true in function, school is in passing time, this line resets.
+        noSchool = false;//If set to tru in function, is before or after school, this line resets.
+
+        if ((currentHour < 7) || ((currentHour == 7) && (currentMinute < 30)) || (currentHour > 14) || ((currentHour == 14) && (currentMinute >= 15))) {
+            noSchool = true;
+            return -1;
+        }
+
+        int[][] periodTimes = //CHANGE BELOW TIMES WHEN SCHEDULE CHANGES
+                {{7, 8, 9, 10, 12, 13},//START HOUR
+                        {30, 25, 50, 45, 30, 25},//START MINUTE
+
+                        {8, 9, 10, 12, 13, 14},//END HOUR
+                        {25, 45, 40, 25, 20, 15}};//END MINUTE
+
+        while (true) {
+            if ((currentHour > periodTimes[2][i])) {
+                i++;
+            } else if ((currentHour == periodTimes[2][i]) && (currentMinute > periodTimes[3][i])) {
+                i++;
+            } else if ((currentHour == periodTimes[0][i]) && (currentMinute > periodTimes[1][i])) {
+                break;
+            } else if (currentHour > periodTimes[0][i]) {
+                break;
+            } else {
+                passingTime = true;
+                return -1;
+            }
+        }
+        return (i + 1);
     }
+
+
+
 
 
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-
-        //MainPage.data.setText(this.dataParsed);
+//set text here
+        //MainActivity.data.setText(this.dataParsed);
 
     }
 }
-///END CODE ATTRIBUTION HERE///
+
--- app/src/main/java/owenspangler/stapleshighschoolscheduler/MainPage.java
+++ app/src/main/java/owenspangler/stapleshighschoolscheduler/MainPage.java
@@ -1,116 +1,38 @@
 package owenspangler.stapleshighschoolscheduler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Calendar;
-//import java.util.Arrays;
+import java.util.Arrays;
 
 
 public class MainPage extends AppCompatActivity {
     //Button click;
     //public  static TextView data;
+    //MyAsyncTask asyncTask = new MyAsyncTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-//get rid of below code and put in separate functions with controls for -1
+
         JSONfetcher process = new JSONfetcher();
         process.execute();
-        ///
-        /*
-        int temp = PeriodNumber();
-        int[] tempsced = ScheduleFormat('b');
-        //int periodforreals = tempsced[temp - 1];
-        //String tempstring = Integer.toString(periodforreals);
-        */
-        String tempstring = process.dayletter;
-        Log.d("watwat",process.dayletter);
-                //Arrays.toString(process.getNewscheduleformat());
+        //String tempstring = jsonDayLetter;
+        //Arrays.toString(process.getNewscheduleformat());
         TextView textView1 = findViewById(R.id.textView1);
-        textView1.setText(tempstring);
+        //textView1.setText(1);
 ////
 
     }
-////ESTABLISH CONNECTION
 
+    //protected void onPostExecute()
 
-    ///
-    //Public Vars
-    char dayLetter = 'z';
-   // String temperature = JSONObject(html).getString("name");///////FIX AND ADD HTML CODE
-    //TimeZone tz;
-    boolean passingTime = false;
-    boolean noSchool = false;
-
-    Calendar cal = Calendar.getInstance();
-    int currentDayNum = cal.get(Calendar.DAY_OF_MONTH);
-    int currentDayDay = cal.get(Calendar.DAY_OF_WEEK);
-    int currentMonth = cal.get(Calendar.MONTH);
-
-    int currentHour = cal.get(Calendar.HOUR_OF_DAY);
-    //int currentHour = 8;
-    int currentMinute = cal.get(Calendar.MINUTE);
-    //int currentMinute = 15;
-    int currentSecond = cal.get(Calendar.SECOND);
-
-
-    int[] ScheduleFormat(char inputDayType) {//edit here for schedule changes
-
-        if (inputDayType == 'a') {
-            int[] tempA = {1, 2, 3, 5, 8, 7}; //A day
-            return tempA;
-        } else if (inputDayType == 'b') {
-            int[] tempB = {2, 3, 4, 6, 7, 8}; //B day
-            return tempB;
-        } else if (inputDayType == 'c') {
-            int[] tempC = {3, 4, 1, 7, 6, 5}; //C day
-            return tempC;
-        } else if (inputDayType == 'd') {
-            int[] tempD = {4, 1, 2, 8, 5, 6}; //D day
-            return tempD;
-        } else {
-            //CALL FUNCTION TO PULL DATA FROM SERVER HERE
-            return null;
-        }
-    }
-
-    int PeriodNumber() {
-        int i = 0; //array position
-        passingTime = false;//If set to true in function, school is in passing time, this line resets.
-        noSchool = false;//If set to tru in function, is before or after school, this line resets.
-
-        if ((currentHour < 7) || ((currentHour == 7) && (currentMinute < 30)) || (currentHour > 14) || ((currentHour == 14) && (currentMinute >= 15))) {
-            noSchool = true;
-            return -1;
-        }
-
-        int[][] periodTimes = //CHANGE BELOW TIMES WHEN SCHEDULE CHANGES
-                {{7, 8, 9, 10, 12, 13},//START HOUR
-                 {30, 25, 50, 45, 30, 25},//START MINUTE
-
-                 {8, 9, 10, 12, 13, 14},//END HOUR
-                 {25, 45, 40, 25, 20, 15}};//END MINUTE
-
-        while (true) {
-            if ((currentHour > periodTimes[2][i])) {
-                i++;
-            } else if ((currentHour == periodTimes[2][i]) && (currentMinute > periodTimes[3][i])) {
-                i++;
-            } else if ((currentHour == periodTimes[0][i]) && (currentMinute > periodTimes[1][i])) {
-                break;
-            } else if (currentHour > periodTimes[0][i]) {
-                break;
-            } else {
-                passingTime = true;
-                return -1;
-            }
-        }
-        return (i + 1);
-    }
 }
