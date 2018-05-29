3e2b2d1753f6c75933a2b8ba361b8441a6d42967
ospangler <39440006+ospangler@users.noreply.github.com>
2018-05-28 20:50:51 -0400

    Everything is working
    Put shit in right class, and now stuff isn't broken anymore. Yay.

--- app/src/main/java/owenspangler/stapleshighschoolscheduler/JSONfetcher.java
+++ app/src/main/java/owenspangler/stapleshighschoolscheduler/JSONfetcher.java
@@ -1,12 +1,10 @@
 package owenspangler.stapleshighschoolscheduler;
-///
+
 import android.os.AsyncTask;
 import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
-
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,25 +12,22 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.net.HttpURLConnection;
+import java.util.Arrays;
 import java.util.Calendar;
-///
-///Portions of Below Code is derived from code made by Abhishek Panwar, 2017///
-///The original code can be found at https://github.com/panwarabhishek345/Receive-JSON-Data///
 
 class JSONfetcher extends AsyncTask<Void,Void,Void> {
+    ///Vars Below///
     String data = "";
-    ////
+
     String jsonDayLetter = "";
     int jsonMonth = -1;
     int jsonDay = -1;
     int[] jsonNewScheduleFormat;
-    int[][] jsonStartTimes;
-    int[][] jsonEndTimes;
+   // int[][] jsonStartTimes;
+   // int[][] jsonEndTimes;
+    int[][] jsonPeriodTimes;
     String jsonNotice;
 
-
-    // String temperature = JSONObject(html).getString("name");///////FIX AND ADD HTML CODE
-    //TimeZone tz;
     boolean passingTime = false;
     boolean noSchool = false;
 
@@ -40,18 +35,12 @@ class JSONfetcher extends AsyncTask<Void,Void,Void> {
     int currentDayNum = cal.get(Calendar.DAY_OF_MONTH);
     int currentDayDay = cal.get(Calendar.DAY_OF_WEEK);
     int currentMonth = cal.get(Calendar.MONTH);
-
     int currentHour = cal.get(Calendar.HOUR_OF_DAY);
     //int currentHour = 8;
     int currentMinute = cal.get(Calendar.MINUTE);
     //int currentMinute = 15;
     int currentSecond = cal.get(Calendar.SECOND);
-
-
-
-
-    ///
-
+    ///End Vars Section///
 
     @Override
     protected Void doInBackground(Void...voids) {
@@ -61,7 +50,11 @@ class JSONfetcher extends AsyncTask<Void,Void,Void> {
 
         return null;///redundant
     }
+
 void RetrieveJson(){
+        ///START CODE ATTRIBUTION HERE///
+        ///Portions of Below Code is from code made by Abhishek Panwar, 2017
+        ///The original code can be found at https://github.com/panwarabhishek345/Receive-JSON-Data
     try {
         URL url = new URL("https://ospangler.github.io/schedulechangedatabase.json");
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
@@ -77,37 +70,48 @@ void RetrieveJson(){
             data = data + line;
         }
 
-
     } catch (MalformedURLException e) {
         e.printStackTrace();
     } catch (IOException e) {
         e.printStackTrace();
     }
+    ///END CODE ATTRIBUTION HERE///
 }
+
     void GetInfoFromJSON(String inputdata){
         try {
             JSONObject JO = new JSONObject(inputdata);
             jsonDayLetter= JO.getString("dayletter");
             jsonMonth = JO.getInt("month");
             jsonDay = JO.getInt("day");
-            ////
-            // Retrieve number array from JSON object "newscheduleformat".
-            JSONArray NSFarray = JO.optJSONArray("newscheduleformat");
-            if (NSFarray == null) { /*ENTER SOME ERROR CODE HERE*/ }
-            jsonNewScheduleFormat = new int[NSFarray.length()];
-            for (int i = 0; i < NSFarray.length(); ++i) {
-                jsonNewScheduleFormat[i] = NSFarray.optInt(i);
-            }
-            ///
-            int[] tempJsonStartTimesHour = getArrayFromJSON("startimeshour");
+
+            jsonNewScheduleFormat = getArrayFromJSON("newscheduleformat");
+            int[] tempJsonStartTimesHour = getArrayFromJSON("starttimeshour");
             int[] tempJsonStartTimesMinute = getArrayFromJSON("starttimesminute");
             int[] tempJsonEndTimesHour = getArrayFromJSON("endtimeshour");
             int[] tempJsonEndTimesMinute = getArrayFromJSON("endtimesminute");
 
+            jsonPeriodTimes = new int[3][tempJsonStartTimesHour.length];
+            for(int i = 0; i<3; i++){
+                for(int j = 0; j<tempJsonStartTimesHour.length; j++){
+                    if(i == 0) {
+                        jsonPeriodTimes[i][j] = tempJsonStartTimesHour[j];
+                    }else if(i==1){
+                        jsonPeriodTimes[i][j] = tempJsonStartTimesMinute[j];
+                    }else if(i==2){
+                        jsonPeriodTimes[i][j] = tempJsonEndTimesHour[j];
+                    }else if(i==3){
+                        jsonPeriodTimes[i][j] = tempJsonEndTimesMinute[j];
+                    }
+                }
+
+            }
+            Log.i("wwwww", Arrays.deepToString(jsonPeriodTimes));
         }catch (JSONException e) {
             e.printStackTrace();
         }
     }
+
     int[] getArrayFromJSON(String jsonID) {
         try {
             JSONObject tempJO = new JSONObject(data);
@@ -122,21 +126,21 @@ void RetrieveJson(){
             e.printStackTrace();
             return null;
         }
-        //return null;
     }
-    int[] ScheduleFormat(char inputDayType) {//edit here for schedule changes
 
-        if (inputDayType == 'a') {
-            int[] tempA = {1, 2, 3, 5, 8, 7}; //A day
+    int[] ScheduleFormat(String inputDayType) { //edit here for schedule changes
+
+        if (inputDayType.equals("a")) {
+            int[] tempA = {1, 2, 3, 5, 8, 7}; //'A' day
             return tempA;
-        } else if (inputDayType == 'b') {
-            int[] tempB = {2, 3, 4, 6, 7, 8}; //B day
+        } else if (inputDayType.equals("b")) {
+            int[] tempB = {2, 3, 4, 6, 7, 8}; //'B' day
             return tempB;
-        } else if (inputDayType == 'c') {
-            int[] tempC = {3, 4, 1, 7, 6, 5}; //C day
+        } else if (inputDayType.equals("c")) {
+            int[] tempC = {3, 4, 1, 7, 6, 5}; //'C' day
             return tempC;
-        } else if (inputDayType == 'd') {
-            int[] tempD = {4, 1, 2, 8, 5, 6}; //D day
+        } else if (inputDayType.equals("d")) {
+            int[] tempD = {4, 1, 2, 8, 5, 6}; //'D' day
             return tempD;
         } else {
             //CALL FUNCTION TO PULL DATA FROM SERVER HERE
@@ -144,10 +148,10 @@ void RetrieveJson(){
         }
     }
 
-    int PeriodNumber() {
+    int PeriodNumber() { //NOTE: ADD SAFEGUARDS TO PREVENT ARRAY READ AT -1!!!!!
         int i = 0; //array position
         passingTime = false;//If set to true in function, school is in passing time, this line resets.
-        noSchool = false;//If set to tru in function, is before or after school, this line resets.
+        noSchool = false;//If set to true in function, is before or after school, this line resets.
 
         if ((currentHour < 7) || ((currentHour == 7) && (currentMinute < 30)) || (currentHour > 14) || ((currentHour == 14) && (currentMinute >= 15))) {
             noSchool = true;
@@ -175,19 +179,14 @@ void RetrieveJson(){
                 return -1;
             }
         }
-        return (i + 1);
+        return (i + 1);//returns period number, must subtract one to get proper array position
     }
-
-
-
-
 
     @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
-//set text here
-        //MainActivity.data.setText(this.dataParsed);
-
+        //set text here and do some cool stuff here later
+        //runs when program is finished
     }
 }
 
--- app/src/main/java/owenspangler/stapleshighschoolscheduler/MainPage.java
+++ app/src/main/java/owenspangler/stapleshighschoolscheduler/MainPage.java
@@ -1,38 +1,24 @@
 package owenspangler.stapleshighschoolscheduler;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.Calendar;
 import java.util.Arrays;
 
-
 public class MainPage extends AppCompatActivity {
-    //Button click;
-    //public  static TextView data;
-    //MyAsyncTask asyncTask = new MyAsyncTask();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
-
         JSONfetcher process = new JSONfetcher();
         process.execute();
-        //String tempstring = jsonDayLetter;
-        //Arrays.toString(process.getNewscheduleformat());
-        TextView textView1 = findViewById(R.id.textView1);
-        //textView1.setText(1);
-////
-
     }
-
-    //protected void onPostExecute()
-
+    //Add post execute here later
 }
